ed50ac24f2e310343bd6da615c548c56d65236a2
mitsunaga <sauimff27@yahoo.co.jp>
2025-02-02 19:23:49 +0900

    SSG化

--- app/(blog)/[slug]/page.tsx
+++ app/(blog)/[slug]/page.tsx
@@ -1,14 +1,18 @@
-import { getArticle } from "@/lib/microcms";
+import { getArticle, getArticles } from "@/lib/microcms";
 import { Article } from "@/types/article";
 import ArticleDetail from "@/components/ArticleDetail";
 
-export default async function ArticlePage({
-  params,
-}: {
-  params: { slug: string };
+export default async function ArticlePage(props: {
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params;
+  const { slug } = await props.params; // `await` で `params` を展開
   const article: Article = await getArticle(slug);
 
   return <ArticleDetail article={article} />;
+}
+
+// SSGで動的パスを生成
+export async function generateStaticParams(): Promise<{ slug: string }[]> {
+  const { contents } = await getArticles();
+  return contents.map((article) => ({ slug: article.id }));
 }
